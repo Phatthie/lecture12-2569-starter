4cde8d57d856dc4885a96ec2d5ea3b4d7ecaab6a
Phatthie <younefayya@gmail.com>
2026-09-03 11:24:30 +0700

    a

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,5 +1,32 @@
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import TaskInput from "../components/TaskInput";
+import Footer from "../components/Footer";
+import TaskCard from "../components/TaskCard";
+import { type TaskCardProps } from "../libs/Todolist";
 export default function TodolistPage() {
+  // 📋 src/pages/TodolistPage.tsx
+  const title = "Todo List";
+  const tasks:TaskCardProps[] = [
+    {
+      id: "1",
+      title: "Read a book",
+      description: "Vite + React + Bootstrap + TS",
+      isDone: false,
+    },
+    {
+      id: "2",
+      title: "Write code",
+      description: "Finish project for class",
+      isDone: false,
+    },
+    {
+      id: "3",
+      title: "Deploy app",
+      description: "Push project to GitHub Pages",
+      isDone: true,
+    },
+  ];
   return (
     <div className="container-fluid min-vh-100">
       <div className="row h-100">
@@ -11,32 +38,45 @@ export default function TodolistPage() {
           {/* ══ [ขั้นที่ 2A ] แยก Component "Header" (ไม่มี props) ══════════════════
           <Header />
           */}
-          <header className="text-white bg-secondary p-2 w-100">
+          {/* <header className="text-white bg-secondary p-2 w-100">
             <p className="m-4">lecture12-2569</p>
-          </header>
-
+          </header> */}
+          
+          <Header/>
           <main className="flex-grow-1 p-4">
             <div className="container">
               {/* [ขั้นที่ 1] เปลี่ยน 2 บรรทัดนี้ให้ดึงค่าจากตัวแปรผ่าน { } */}
-              <h2 className="text-center">Todo List</h2>
-              <p className="text-center text-muted">ทั้งหมด ... งาน</p>
+              <h2 className="text-center">{title}</h2>
+              <p className="text-center text-muted">เฉพาะ ID {tasks[0].id} งาน</p>
+              <p className="text-center text-muted">ทั้งหมด {tasks.length} งาน</p>
               {/* ══ [ขั้นที่ 2B] แยก Component "TaskInput" (ไม่มี props) ══════════
                   <TaskInput />
               */}
-              <div className="d-flex my-3">
+              {/* <div className="d-flex my-3">
                 <input
                   className="form-control me-2"
                   type="text"
                   placeholder="Insert a task here.."
                 />
                 <button className="btn btn-primary px-4">Add</button>
-              </div>
-
+              </div> */}
+              <TaskInput/>
+              {
+                tasks.map((task) => (
+                  <TaskCard
+                    key={task.id}
+                    id={task.id}
+                    title={task.title}
+                    description={task.description}
+                    isDone={task.isDone}
+                  />
+                ))};
+              
               {/* ══ [ขั้นที่ 4] แยก Component "TaskCard" + ส่ง props + .map() ═════
                  {tasks.map((task) => <TaskCard key={task.id} {...} />)}
               */}
-              <div className="card mb-3">
-                <div className="card-body">
+              {/* <div className="card mb-3">
+                {/* <div className="card-body">
                   <div className="row align-items-center">
                     <div className="col-lg-4">
                       <h5 className="card-title m-0">Read a book</h5>
@@ -53,11 +93,11 @@ export default function TodolistPage() {
                       <button className="btn btn-danger w-100">Delete</button>
                     </div>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div>
 
               <div className="card mb-3">
-                <div className="card-body">
+                {/* <div className="card-body">
                   <div className="row align-items-center">
                     <div className="col-lg-4">
                       <h5 className="card-title m-0">Write code</h5>
@@ -74,11 +114,11 @@ export default function TodolistPage() {
                       <button className="btn btn-danger w-100">Delete</button>
                     </div>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div> */}
 
-              <div className="card mb-3">
-                <div className="card-body">
+              {/* <div className="card mb-3"> */}
+                {/* <div className="card-body">
                   <div className="row align-items-center">
                     <div className="col-lg-4">
                       <h5 className="card-title m-0 text-decoration-line-through">
@@ -97,18 +137,20 @@ export default function TodolistPage() {
                       <button className="btn btn-danger w-100">Delete</button>
                     </div>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div> */}
             </div>
+
           </main>
           {/* ══ [ขั้นที่ 3] แยก Component "Footer" + ส่ง props ════════
                <Footer year="2026" fullName="..." studentId="..." />
           */}
-          <footer className="text-center w-100">
+          {/* <footer className="text-center w-100">
             <p className="text-white bg-secondary p-4 m-0">
               Copyright © 2026 chanadda thanyaratthanon 6706200999
             </p>
-          </footer>
+          </footer> */}
+          <Footer year="2026" fullName="loly poly" studentId="676767676"/>
         </div>
       </div>
     </div>
